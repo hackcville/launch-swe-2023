bda7a5b4df8e3de412d4491384200c0adcdbb38a
RonRan123 <ronith.ranjan123@gmail.com>
2023-05-31 14:02:39 -0400

    spotify-example starting code

---
+++ spotify-example/frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SpotifyApp from "./components/SpotifyApp";
+
+function App() {
+  return (
+    <div className="App">
+      <SpotifyApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ spotify-example/frontend/src/components/SpotifyApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SpotifyApp() {
+  return (
+    <>
+      <h1>Spotify Example App</h1>
+      <button>Log In</button>
+    </>
+  );
+}
+
+export default SpotifyApp;
